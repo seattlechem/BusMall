bc43c6de3688cb64f3a13caddd0c3b6e2f531da7
Peter Kim <seattlechem@yahoo.com>
2018-01-23 16:08:38 -0800

    chart graph works

--- js/app.js
+++ js/app.js
@@ -125,12 +125,13 @@ function imgClickEvent(event){
     }
   }
 
-  if(totalNumOfClicks > 24){
+  if(totalNumOfClicks > 5){
     sectionEl.removeEventListener('click', imgClickEvent);
     console.log('reached 25 clicks');
     sectionEl.innerHTML = '';
-    showTable();
-    percetClickPerItem;
+    // showTable();
+    percetClickPerItem();
+    showChart();
   }
 
 }
@@ -185,12 +186,14 @@ function percetClickPerItem(){
   for(i in objStore){
     var calc = objStore[i].numClicked / totalNumOfClicks * 100;
     percentClickPerItemArray.push(calc);
+    console.log(calc);
 
   }
 }
 
 function showChart(){
-  var canvasEl = document.createElement('canvas').getContext('2d');
+  var canvasEl = document.createElement('canvas');
+  canvasEl.getContext('2d');
 
   var itemChart = new Chart(canvasEl, {
     type: 'bar',
@@ -199,10 +202,37 @@ function showChart(){
       datasets: [{
         label: 'Percentage of Clicks Per Item', //chart title
         data: percentClickPerItemArray,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
       }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
     }
-  });
 
+  });
+  sectionEl.appendChild(canvasEl);
 
 }
 
